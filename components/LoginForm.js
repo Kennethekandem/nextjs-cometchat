c4b6da6c0f0ebbae50e75f1543f7e7c078429fe6
Kenneth Ekandem <kennyekandem@gmail.com>
2021-06-28 14:07:03 +0100

    update to login form

--- components/LoginForm.js
+++ components/LoginForm.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import 'bootstrap/dist/css/bootstrap.css'
 
 class LoginForm extends React.Component {
 
@@ -18,11 +19,26 @@ class LoginForm extends React.Component {
 
     render() {
         return (
-            <form onSubmit={ this.handleSubmit }>
-                <div><input type="email" name="email" placeholder="email" onChange={ this.handleChange } /></div>
-                <div><input type="password" name="password" placeholder="Password" onChange={ this.handleChange } /></div>
-                <button type="submit">Login</button>
-            </form>  
+            <div class="login-form">
+                <div class="row">
+                    <div class="col-6">
+                <div class="card-body">
+                    <form onSubmit={ this.handleSubmit }>
+
+                <div class="form-floating mb-3">
+                    <input type="email" class="form-control" id="floatingInput" name="email" onChange={ this.handleChange } placeholder="name@example.com" />
+                    <label for="floatingInput">Email address</label>
+                </div>
+                <div class="form-floating">
+                    <input type="password" name="password" class="form-control" id="floatingPassword" onChange={ this.handleChange } placeholder="Password" />
+                    <label for="floatingPassword">Password</label>
+                </div> 
+<button class="btn btn-primary" type="submit">Login</button>
+            </form> 
+                </div>
+            </div>
+                </div>
+            </div> 
         )
     }
 }
